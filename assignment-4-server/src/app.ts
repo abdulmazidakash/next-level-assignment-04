9927aec0ac7e13c4c3aa61483093a58c163fbc92
akashabdulmazid <akashabdulmazid@gmail.com>
2026-02-22 10:31:16 +0600

    install: auth and user modules cli install

--- assignment-4-server/src/app.ts
+++ assignment-4-server/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { AuthRoutes } from './modules/Auth/auth.route';
 
 const app: Application = express();
 
@@ -9,9 +10,11 @@ app.use(cors());
 
 // application routes
 // app.use('/api/v1', router);
+app.use('/api/v1/auth', AuthRoutes)
+
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Apollo Gears World!');
+  res.send('Hello from FoodHub World!');
 });
 
 export default app;
